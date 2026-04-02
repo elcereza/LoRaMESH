96e819d45ab7facb27ce401ebb453cb33709bd6d
Gustavo Cereza <gustavo.cereza7@gmail.com>
2026-04-02 10:45:05 -0300

    fix: align Linux UART handling with Windows (DTR/RTS, raw mode, timeout, buffer flush)

--- python/src/loramesh/adapters/linux/LinuxSerialTransport.cpp
+++ python/src/loramesh/adapters/linux/LinuxSerialTransport.cpp
@@ -5,17 +5,53 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <time.h>
+#include <string.h>
+
+static speed_t getBaud(int baud)
+{
+    switch (baud)
+    {
+        case 9600: return B9600;
+        case 19200: return B19200;
+        case 38400: return B38400;
+        case 57600: return B57600;
+        case 115200: return B115200;
+        default: return B9600;
+    }
+}
 
 LinuxSerialTransport::LinuxSerialTransport(const char* device, int baud)
 {
-    fd = open(device, O_RDWR | O_NOCTTY | O_SYNC);
+    fd = open(device, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        return;
+    }
+
+    int flags;
+    ioctl(fd, TIOCMGET, &flags);
+    flags |= TIOCM_DTR;
+    flags |= TIOCM_RTS;
+    ioctl(fd, TIOCMSET, &flags);
+
+    usleep(200000);
 
     struct termios tty;
-    tcgetattr(fd, &tty);
+    memset(&tty, 0, sizeof tty);
 
-    cfsetospeed(&tty, B9600);
-    cfsetispeed(&tty, B9600);
+    if (tcgetattr(fd, &tty) != 0)
+    {
+        close(fd);
+        fd = -1;
+        return;
+    }
 
+    speed_t speed = getBaud(baud);
+
+    cfsetospeed(&tty, speed);
+    cfsetispeed(&tty, speed);
+
+    // 8N1
     tty.c_cflag |= (CLOCAL | CREAD);
     tty.c_cflag &= ~CSIZE;
     tty.c_cflag |= CS8;
@@ -24,7 +60,15 @@ LinuxSerialTransport::LinuxSerialTransport(const char* device, int baud)
     tty.c_cflag &= ~CSTOPB;
     tty.c_cflag &= ~CRTSCTS;
 
+    tty.c_lflag = 0;
+    tty.c_oflag = 0;
+    tty.c_iflag = 0;
+
+    tty.c_cc[VMIN]  = 0;
+    tty.c_cc[VTIME] = 5; // 0.5s
+
     tcsetattr(fd, TCSANOW, &tty);
+    tcflush(fd, TCIOFLUSH);
 }
 
 bool LinuxSerialTransport::begin()
@@ -34,17 +78,21 @@ bool LinuxSerialTransport::begin()
 
 size_t LinuxSerialTransport::write(const uint8_t* data, size_t len)
 {
+    if (fd < 0) return 0;
     return ::write(fd, data, len);
 }
 
 size_t LinuxSerialTransport::read(uint8_t* data, size_t len)
 {
+    if (fd < 0) return 0;
     return ::read(fd, data, len);
 }
 
 size_t LinuxSerialTransport::available()
 {
-    int bytes;
+    if (fd < 0) return 0;
+
+    int bytes = 0;
     ioctl(fd, FIONREAD, &bytes);
     return bytes;
 }
@@ -55,4 +103,5 @@ uint32_t LinuxSerialTransport::millis()
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
 }
+
 #endif
